530f9373520913695ff612be6f50011a1eac2e5b
Eric Peterson <ericpeterson91@live.ca>
2022-10-16 19:55:09 -0600

    hard coded questions

--- game.js
+++ game.js
@@ -12,35 +12,93 @@ let availableQuestions = [];
 
 let questions = [
   {
-    question: "Inside which HTML element do we put the JavaScript??",
-    choice1: "<script>",
-    choice2: "<javascript>",
-    choice3: "<js>",
-    choice4: "<scripting>",
+    question: "What is the capital of Saskatchewan?",
+    choice1: "Saskatoon",
+    choice2: "Regina",
+    choice3: "Moose Jaw",
+    choice4: "Prince Albert",
+    answer: 2
+  },
+  {
+    question:
+      "Which area in Canada is famous for houseboating?",
+    choice1: "The Pas",
+    choice2: "Niagara On the Lake",
+    choice3: "Shuswap",
+    choice4: "Banff",
+    answer: 3
+  },
+  {
+    question: "Which is these celebrities is from Vancouver?",
+    choice1: "Ryan Gosling",
+    choice2: "Nicki Minaj",
+    choice3: "Sebastian Maniscalco",
+    choice4: "Ryan Reynolds",
+    answer: 4
+  },
+  {
+    question: "Approximately how many people visit Banff national park per year?",
+    choice1: "1 million",
+    choice2: "2 million",
+    choice3: "4 million",
+    choice4: "8 million",
+    answer: 3
+  },
+  {
+    question:
+      "In what year did Nunavut become a territory?",
+    choice1: "1965",
+    choice2: "1980",
+    choice3: "1991",
+    choice4: "1999",
+    answer: 4
+  },
+  {
+    question: "Which Canadian city has the second largest population?",
+    choice1: "Vancouver",
+    choice2: "Toronto",
+    choice3: "Calgary",
+    choice4: "Montreal",
+    answer: 4
+  },
+  {
+    question: "Which province has the largest GDP per capita?",
+    choice1: "Alberta",
+    choice2: "Quebec",
+    choice3: "Ontario",
+    choice4: "British Columbia",
     answer: 1
   },
   {
     question:
-      "What is the correct syntax for referring to an external script called 'xxx.js'?",
-    choice1: "<script href='xxx.js'>",
-    choice2: "<script name='xxx.js'>",
-    choice3: "<script src='xxx.js'>",
-    choice4: "<script file='xxx.js'>",
-    answer: 3
+      "What percentage of Canadians live within 160 kilometers of the U.S. border?",
+    choice1: "50%",
+    choice2: "65%",
+    choice3: "80%",
+    choice4: "90%",
+    answer: 4
   },
   {
-    question: " How do you write 'Hello World' in an alert box?",
-    choice1: "msgBox('Hello World');",
-    choice2: "alertBox('Hello World');",
-    choice3: "msg('Hello World');",
-    choice4: "alert('Hello World');",
-    answer: 4
+    question: "According to Apple Maps, how long would it take to drive from Vancouver, BC to St.John's, NL? (without stopping)",
+    choice1: "2 days, 16 hours",
+    choice2: "3 days, 4 hours",
+    choice3: "3 days, 20 hours",
+    choice4: "4 days, 4 hours",
+    answer: 2
+  },
+  {
+    question: "What percentage of Canadians speak French?",
+    choice1: "15%",
+    choice2: "21%",
+    choice3: "26%",
+    choice4: "33%",
+    answer: 2
   }
 ];
 
 //CONSTANTS
 const CORRECT_BONUS = 10;
-const MAX_QUESTIONS = 3;
+const MAX_QUESTIONS = 10;
 
 startGame = () => {
   questionCounter = 0;
